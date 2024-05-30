db847b112cf4da8718f1ce2caa27842562650735
Keval Vasani <kevalv.tagline@gmail.com>
2024-05-30 10:35:40 +0530

    okgfm

--- Blogs/admin.py
+++ Blogs/admin.py
@@ -3,14 +3,28 @@ from Blogs.models import *
 
 # Register your models here.
 
+
+
 @admin.register(User)
-class User(admin.ModelAdmin):
+class User_Admin(admin.ModelAdmin):
     list_display=['username','first_name','email','biography', 'is_blogger']
 
 @admin.register(Blogs)
-class Post(admin.ModelAdmin):
+class Blogs_Admin(admin.ModelAdmin):
     list_display=['title','description','author','created_at','updated_at']
 
 @admin.register(Comments)
-class Post(admin.ModelAdmin):
+class Comments_Admin(admin.ModelAdmin):
     list_display=['comment','user','blog','created_at','updated_at']
+
+class Blog_Comments_Inline(admin.TabularInline):
+    model = Comments
+    extra = 0
+    
+class Blogs_Admin(admin.ModelAdmin):
+    inlines = [Blog_Comments_Inline]
+
+    class Meta:
+        model = Blogs
+    
+admin.site.register(Blogs_Admin,Blog_Comments_Inline)
