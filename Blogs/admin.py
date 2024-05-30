6493995b876567a063f4e2c4ab507dd1a75d2716
Keval Vasani <kevalv.tagline@gmail.com>
2024-05-30 10:51:33 +0530

    dgfdgd

--- Blogs/admin.py
+++ Blogs/admin.py
@@ -8,26 +8,14 @@ from Blogs.models import *
 class User_Admin(admin.ModelAdmin):
     list_display=['username','first_name','email','biography', 'is_blogger']
 
+class Blog_Comments_Inline(admin.TabularInline):
+    model = Comments
+
 @admin.register(Blogs)
 class Blogs_Admin(admin.ModelAdmin):
     list_display=['title','description','author','created_at','updated_at']
+    inlines = [Blog_Comments_Inline]
 
 @admin.register(Comments)
 class Comments_Admin(admin.ModelAdmin):
     list_display=['comment','user','blog','created_at','updated_at']
-
-class Blog_Comments_Inline(admin.TabularInline):
-    model = Comments
-    
-class Blogs_Admin(admin.ModelAdmin):
-    inlines = [Blog_Comments_Inline]
-
-    class Meta:
-        model = Blogs
-    
-admin.site.register(Blogs_Admin,Blog_Comments_Inline)
-
-
-# Admin site blog posts records should display the list of associated comments inline (below each blog post).
-# TypeError: 'MediaDefiningClass' object is not iterable
-
